3ff8339742263178e6719a336c36ec0483b03e15
Adla Vigneshwar <97793513+vigneshwar999@users.noreply.github.com>
2026-09-06 08:12:35 +0000

    Restored to 223e4d9fff66da679c0fd4ca15c3dca9d5443a5c

--- tmp_serve.js
+++
@@ -1,1 +0,0 @@
-import { serve } from 'bun';\nimport { createReadStream } from 'fs';\nserve({ port: 3000, fetch() { return new Response(createReadStream('artifacts/envelo-human-value/src/styles/index.css')) } });
